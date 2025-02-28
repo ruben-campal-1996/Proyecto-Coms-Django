a6ef038a6a1ee2368d036a9a9eed8e1859f6c8f2
ruben.campal <putosnombre@gmail.com>
2025-02-28 08:24:22 +0100

    Actualización base de datos proyecto, no debía estar aquí

--- proyectocomunicaciones/Proyectos/models.py
+++ proyectocomunicaciones/Proyectos/models.py
@@ -1,11 +1,1 @@
-from django.db import models
 
-class Proyecto(models.Model):
-    titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    fecha_inicio = models.DateField()
-    fecha_finalizacion = models.DateField()
-    usuarios = models.ManyToManyField('UsersAutentication.Usuario', related_name="proyectos_asociados")  # Cambia el related_name
-
-    def __str__(self):
-        return self.titulo
