ed40bedb193eb0952b96b0e0c16effe1159029c3
ruben.campal <putosnombre@gmail.com>
2025-02-28 09:29:50 +0100

    Implementacion de bases de datos inicial + Proyecto/porcentaje tareas completado

--- proyectocomunicaciones/Tareas/models.py
+++ proyectocomunicaciones/Tareas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Tarea(models.Model):
+    ESTADOS = [
+        ('sin_empezar', 'Sin empezar'),
+        ('en_proceso', 'En proceso'),
+        ('finalizada', 'Finalizada'),
+    ]
+
+    id_tarea = models.AutoField(primary_key=True)
+    tarea = models.CharField(max_length=255)
+    usuarios_asignados = models.ManyToManyField('UsersAutentication.Usuario', related_name='tareas_asignadas')
+    estado = models.CharField(max_length=20, choices=ESTADOS)
+    proyecto = models.ForeignKey('Proyectos.Proyecto', on_delete=models.CASCADE, related_name='tareas')
+
+    def __str__(self):
+        return self.tarea
--- proyectocomunicaciones/UsersAutentication/models.py
+++ proyectocomunicaciones/UsersAutentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    ROLES = [
+        ('invitado', 'Invitado'),
+        ('miembro', 'Miembro'),
+        ('administrador', 'Administrador'),
+    ]
+    
+    id_user = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    rol = models.CharField(max_length=20, choices=ROLES)
+    proyectos = models.ManyToManyField('Proyectos.Proyecto', related_name='usuarios_asociados')  # Relación con proyectos
+    tareas = models.ManyToManyField('Tarea', related_name='usuarios_asignados')  # Relación con tareas
+
+    def __str__(self):
+        return self.nombre
